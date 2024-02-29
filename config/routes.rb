b7206ad9d832d3ece4d2f563e50ebe4d79e12994
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-29 19:28:01 +0000

    Added posts and liked photos routes and pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
+
+  get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed"
+  get ":username/followers"
+  get ":username/following"
+
   get "/:username" => "users#show", as: :user
 
 
